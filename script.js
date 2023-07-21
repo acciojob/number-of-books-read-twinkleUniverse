cb9b3838bd690ce8deb865b8223af64f29d46d3a
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-21 17:28:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+   let count=0;
+	for(let i=0;i<library.length;i++){
+		if(library[i].readingStatus) count++;
+	}
+	 console.log(count);
 };
 
 // Do not change the code below
